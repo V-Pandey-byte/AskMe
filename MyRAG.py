3a76701e340d3f2490dd8e448a1db485b4344290
Vishnu <vishpann@gmail.com>
2024-12-25 13:10:30 -0600

    AI API

---
+++ MyRAG.py
@@ -0,0 +1,20 @@
+# AI API
+import ollama
+model = "llama3.1"
+
+class MyRAG:
+    def chatWithMe (self, query):
+        prompt = query
+        stream = ollama.chat(model=model, messages=[{"role": "user", "content": prompt,}], stream=True)
+        for chunk in stream:
+                print(chunk['message']['content'],end='')
+        print("")
+
+myRag = MyRAG()
+inputQuery = input("Ask me: ")
+while inputQuery != "exit":
+    myRag.chatWithMe(inputQuery)
+    print("*" * 100)
+    inputQuery = input("Ask me: ")
+
+print("Goodbye!")
